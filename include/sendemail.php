e473faf9e81d996a9b8406de791aab52b920475e
georgeslabs <mc-georges@live.fr>
2017-05-12 01:11:14 +0200

    refacto du formulaire de contact en ajax avec spinner

--- include/sendemail.php
+++ include/sendemail.php
@@ -15,7 +15,7 @@ $toemails[1] = array(
 			);
 
 // Form Processing Messages
-$message_success = 'We have <strong>successfully</strong> received your Message and will get Back to you as soon as possible.';
+$message_success = 'response-ok';
 
 // Add this only if you use reCaptcha with your Contact Forms
 $recaptcha_secret = ''; // Your reCaptcha Secret
@@ -62,31 +62,11 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 				$mail->AddAttachment( $_FILES['template-contactform-file']['tmp_name'], $_FILES['template-contactform-file']['name'] );
 			}
 
-			// Runs only when reCaptcha is present in the Contact Form
-			if( isset( $_POST['g-recaptcha-response'] ) ) {
-				$recaptcha_response = $_POST['g-recaptcha-response'];
-				$response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" . $recaptcha_response );
-
-				$g_response = json_decode( $response );
-
-				if ( $g_response->success !== true ) {
-					echo '{ "alert": "error", "message": "Captcha not Validated! Please Try Again." }';
-					die;
-				}
-			}
-
-			// Uncomment the following Lines of Code if you want to Force reCaptcha Validation
-
-			// if( !isset( $_POST['g-recaptcha-response'] ) ) {
-			// 	echo '{ "alert": "error", "message": "Captcha not Submitted! Please Try Again." }';
-			// 	die;
-			// }
-
 			$mail->MsgHTML( $body );
 			$sendEmail = $mail->Send();
 
 			if( $sendEmail == true ):
-				echo '{ "alert": "success", "message": "' . $message_success . '" }';
+				echo $message_success;
 			else:
 				echo '{ "alert": "error", "message": "Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '" }';
 			endif;
